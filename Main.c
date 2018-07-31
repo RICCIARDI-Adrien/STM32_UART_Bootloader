b52890c97d0c556c4d0442edd7a265b79f3177b4
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2018-07-31 18:01:20 +0200

    Added bootloader version retrieval.

--- Main.c
+++ Main.c
@@ -1,5 +1,5 @@
 /** @file Main.c
- * Allow to flash STM32 firmware using the protocol described in ST application note AN3155.
+ * Allow to flash STM32 firmware through a serial port using the protocol described in ST application note AN3155.
  * @author Adrien RICCIARDI
  */
 #include <errno.h>
@@ -23,11 +23,45 @@ static void MainProgramExit(void)
 	SerialPortClose(Serial_Port_ID);
 }
 
+/** Send a command and its payload.
+ * @param Command_Code The command code (see application note for available commands).
+ * @param Pointer_Buffer_Payload Contain the payload data.
+ * @param Payload_Size How many bytes of payload to send.
+ */
+void MainSendCommand(unsigned char Command_Code, unsigned char *Pointer_Buffer_Payload, int Payload_Size)
+{
+	unsigned char Checksum = 0;
+	
+	// Send command code
+	SerialPortWriteByte(Serial_Port_ID, Command_Code);
+	SerialPortWriteByte(Serial_Port_ID, ~Command_Code); // A command code is followed by the complemented code
+	
+	// Send payload (if any)
+	if (Payload_Size > 0)
+	{
+		while (Payload_Size > 0)
+		{
+			SerialPortWriteByte(Serial_Port_ID, *Pointer_Buffer_Payload);
+			
+			// Update checksum
+			Checksum ^= *Pointer_Buffer_Payload;
+			
+			Pointer_Buffer_Payload++;
+			Payload_Size--;
+		}
+		
+		// Send checksum
+		SerialPortWriteByte(Serial_Port_ID, Checksum);
+	}
+}
+
 //-------------------------------------------------------------------------------------------------
 // Entry point
 //-------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
+	unsigned char Buffer[512];
+	
 	// Check parameters
 	if (argc != 3)
 	{
@@ -45,14 +79,19 @@ int main(int argc, char *argv[])
 	atexit(MainProgramExit);
 	
 	// Configure device for using UART
-	printf("Connecting to microcontroller...\n");
+	printf("Waiting for microcontroller...\n");
 	SerialPortWriteByte(Serial_Port_ID, 0x7F);
 	if (SerialPortReadByte(Serial_Port_ID) != 0x79)
 	{
 		printf("Error : bad microcontroller answer. Make sure microcontroller is in bootloader mode and baud rate is not too fast.\n");
 		return EXIT_FAILURE;
 	}
-	printf("Connection successful.\n");
+	printf("Successfully connected to microcontroller.\n");
+	
+	// Display bootloader version
+	MainSendCommand(0x01, NULL, 0);
+	SerialPortReadBuffer(Serial_Port_ID, Buffer, 5);
+	printf("Bootloader version : %d.%d\n", Buffer[1] >> 4, Buffer[1] & 0x0F);
 	
 	return EXIT_SUCCESS;
 }

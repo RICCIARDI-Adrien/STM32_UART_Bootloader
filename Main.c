8f5fe1b67db18fbb7ca06cba92d9d397fe18048a
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2018-07-31 17:02:51 +0200

    Added serial port library as a git submodule. Microcontroller is acknowledging to initialization command.

--- Main.c
+++ Main.c
@@ -2,9 +2,57 @@
  * Allow to flash STM32 firmware using the protocol described in ST application note AN3155.
  * @author Adrien RICCIARDI
  */
+#include <errno.h>
+#include <Serial_Port.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+//-------------------------------------------------------------------------------------------------
+// Private variables
+//-------------------------------------------------------------------------------------------------
+/** The serial port identifier. */
+TSerialPortID Serial_Port_ID;
+
+//-------------------------------------------------------------------------------------------------
+// Private functions
+//-------------------------------------------------------------------------------------------------
+/** Gracefully release the opened serial port on program exit. */
+static void MainProgramExit(void)
+{
+	SerialPortClose(Serial_Port_ID);
+}
+
+//-------------------------------------------------------------------------------------------------
+// Entry point
+//-------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
-	return 0;
+	// Check parameters
+	if (argc != 3)
+	{
+		printf("Usage : %s Serial_Port Firmware_File\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	// Try to open serial port
+	if (SerialPortOpen(argv[1], 1200, SERIAL_PORT_PARITY_EVEN, &Serial_Port_ID) != 0)
+	{
+		printf("Error : could not open '%s' serial port.\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+	// Automatically close serial port on program exit
+	atexit(MainProgramExit);
+	
+	// Configure device for using UART
+	printf("Connecting to microcontroller...\n");
+	SerialPortWriteByte(Serial_Port_ID, 0x7F);
+	if (SerialPortReadByte(Serial_Port_ID) != 0x79)
+	{
+		printf("Error : bad microcontroller answer. Make sure microcontroller is in bootloader mode and baud rate is not too fast.\n");
+		return EXIT_FAILURE;
+	}
+	printf("Connection successful.\n");
+	
+	return EXIT_SUCCESS;
 }
--- Makefile
+++ Makefile
@@ -2,10 +2,11 @@ CC = gcc
 CCFLAGS = -W -Wall
 
 BINARY = stm32-uart-bootloader
-SOURCES = Main.c
+INCLUDES = -ISerial_Port_Library/Includes
+SOURCES = Main.c Serial_Port_Library/Sources/Serial_Port_Linux.c Serial_Port_Library/Sources/Serial_Port_Windows.c
 
 all:
-	$(CC) $(CCFLAGS) $(SOURCES) -o $(BINARY)
+	$(CC) $(CCFLAGS) $(INCLUDES) $(SOURCES) -o $(BINARY)
 
 clean:
 	rm -f $(BINARY)
